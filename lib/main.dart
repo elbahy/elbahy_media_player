16718d450ae556b434c97429c8478cfe27ee0bae
elbahy <vb.supporter@gmail.com>
2022-12-23 16:14:11 +0200

    test play one sound

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 
 void main() {
-  runApp(MaterialApp());
+  runApp(PlayerApp());
+}
+
+final player = AudioPlayer(); // Create a player
+final duration = player.setUrl(// Load a URL
+    'music/01.mp3');
+
+class PlayerApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          body: Column(children: [
+            TextButton(
+                onPressed: () {
+                  player.play();
+                },
+                child: Text("data",
+                    style: TextStyle(
+                        color: Colors.red, fontWeight: FontWeight.bold))),
+          ]),
+        ),
+      ),
+    );
+  }
 }
